25a91697482cc7dd0fd5935308842d1e41e1e1d4
Wagner Esser <waesser@gmail.com>
2019-01-24 03:28:06 -0200

    edição de fornecedor

--- js/controllers/fornecedor/edit.controller.js
+++ js/controllers/fornecedor/edit.controller.js
@@ -1,3 +1,13 @@
-angular.module('App').controller('EditCtrl', function (/* $scope, $location, $http */) {
-    // ...
+angular.module('App').controller('EditCtrl', function ($routeParams, $scope, FornecedorService, $location, $http) {
+
+    $scope.fornecedor = null;
+
+    FornecedorService.getById($routeParams.id).then(function (response) {
+        $scope.fornecedor = response.data;
+    });
+
+    $scope.update = function (fornecedor) {
+        FornecedorService.update(fornecedor, fornecedor.id);
+        $location.path( "/fornecedores" );
+    };
 });
