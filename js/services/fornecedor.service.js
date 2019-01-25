137bef4a9ada98c5981eafe9c478312fb8b54976
Wagner Esser <waesser@gmail.com>
2019-01-25 18:43:54 -0200

    .

--- js/services/fornecedor.service.js
+++ js/services/fornecedor.service.js
@@ -14,7 +14,6 @@ angular.module('App').factory('FornecedorService', function ($http, URL_API) {
             return $http.put(URL_API + id, fornecedor);
         },
         delete: function (id) {
-            console.log("deletadooooooooo");
             return $http.delete(URL_API + id);
         },
         searchByName: function (busca) {
